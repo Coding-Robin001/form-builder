0d67a073418e8db5a0cdead199fdd0e3daf5b8e4
Coding-Robin001 <codingade@gmail.com>
2025-10-29 00:56:06 +0000

    drag designerELement over another designerElement withinv the dropzone and switching postion/places done

--- src/components/designer.tsx
+++ src/components/designer.tsx
@@ -7,7 +7,7 @@ import DesignerElementWrapper from './designElementWrapper'
 
 function Designer() {
 
-    const { elements, addElement, selectedElement, setSelectedElement } = UseDesigner()
+    const { elements, addElement, selectedElement, setSelectedElement, removeElement } = UseDesigner()
 
     const droppable = useDroppable({
         id: "designer-drop-area",
@@ -26,7 +26,7 @@ function Designer() {
 
             const isDroppingSidebarBtnElementOverDesignerDropArea = isDesignerBtnElement && isDroppingOverDesignerDropArea
 
-            // first droppable scenario
+            // 1st droppable scenario (drag sidebarBtnElement over to dropzone to last place/postion) 
             if (isDroppingSidebarBtnElementOverDesignerDropArea) {
                 const type = active?.data?.current?.type
                 const newElement = FormElements[type as ElementsType].construct(idGenerator())
@@ -42,7 +42,7 @@ function Designer() {
             const droppingSideBarBtnOverDesignerElement = isDesignerBtnElement && isDroppingOverDesignerElement
 
 
-            // second droppable scenario
+            // 2nd droppable scenario (drag sidebarBtnElement and drop over designerelement in dropzone)
             if (droppingSideBarBtnOverDesignerElement) {
                 const type = active?.data?.current?.type
                 const newElement = FormElements[type as ElementsType].construct(idGenerator())
@@ -63,6 +63,33 @@ function Designer() {
                 return
             }
 
+
+            // 3rd scenario (drag a designerElement within dropzone over another designerelement changing postions)
+            const isDraggingDesignerElement = active?.data?.current?.isDesignerElement
+            const draggingDesignerElementOverAnotherDesignerElement = isDroppingOverDesignerElement && isDraggingDesignerElement
+
+            if (draggingDesignerElementOverAnotherDesignerElement) {
+                const activeId = active?.data?.current?.elementId
+                const overId = over?.data?.current?.elementId
+
+                const activeElementIndex = elements.findIndex((el) => el.id === activeId)
+                const overElementIndex = elements.findIndex((el) => el.id === overId)
+
+                if (activeElementIndex === -1 || overElementIndex === -1) {
+                    throw new Error("element not found!")
+                }
+
+                const activeElement = { ...elements[activeElementIndex] }
+                removeElement(activeId)
+
+                let indexForNewElement = overElementIndex //assume im on op half
+                if (isDroppingOverDesignerElementBottomHalf) {
+                    indexForNewElement = overElementIndex + 1
+                }
+
+                addElement(indexForNewElement, activeElement)
+
+            }
             console.log("drag end event ", event)
 
         }
@@ -88,10 +115,11 @@ function Designer() {
                 }}
             >
                 <div
-
                     ref={droppable.setNodeRef}
-                    className="p-4 w-[80%] h-[100%] border-2 border-dashed border-gray-700 rounded-2xl  text-gray-400 text-base font-medium  relative flex flex-col items-center justify-start bg-gray-950/40 transition-colors duration-300"
+                    className={`relative flex flex-col items-center justify-start w-[85%] min-h-[100vh] p-6 border-3 border-gray-700 rounded-md bg-gray-950/60 text-gray-300 font-medium transition-all duration-300 ease-in-out
+    ${droppable.isOver ? 'ring-2 ring-inset border-blue-500 bg-gray-900/80 shadow-[0_0_25px_5px_rgba(59,130,246,0.25)] scale-[1.01]' : 'hover:border-gray-600'} `}
                 >
+
                     {/* Center message */}
                     {!droppable.isOver && elements.length === 0 && (
                         <p className="absolute inset-0 flex items-center justify-center text-gray-500 font-semibold tracking-wide">
